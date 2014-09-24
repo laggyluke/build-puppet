cee27959003801d496cf13ebf1cad8b6277c994a
George Miroshnykov <george.miroshnykov@gmail.com>
2014-09-24 18:59:23 +0300

    Initial support for proxxy

---
+++ manifests/proxxy.pp
@@ -0,0 +1,6 @@
+# FIXME: this is a temporary file for local development
+# it should be removed in the final version
+
+node "proxxy.dev" {
+    include toplevel::server::proxxy
+}
---
+++ modules/proxxy/manifests/settings.pp
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class proxxy::settings {
+    $log_dir = "/var/log/proxxy"
+    $cache_dir = "/var/cache/proxxy"
+    $nginx_conf = "/etc/nginx/nginx.conf"
+    $nginx_vhosts_conf = "/etc/nginx/sites-enabled/proxxy"
+
+    $backends = {
+        'ftp.mozilla.org' => {
+            path => 'https://ftp.mozilla.org/',
+            size => '16g',
+            ttl => '1d',
+            username => 'foo',
+            password => 'bar'
+        },
+        'pypi.pub.build.mozilla.org' => {
+            path => 'http://pypi.pub.build.mozilla.org/',
+            size => '16g',
+            ttl => '1d'
+        },
+        's3' => {
+            path => 'https://s3.amazonaws.com/',
+            size => '16g',
+            ttl => '1d'
+        }
+    }
+}
