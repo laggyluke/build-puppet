b318e078c85e8c6b7e6b21b30713a697dad6b799
George Miroshnykov <gmiroshnykov@mozilla.com>
2014-09-25 02:46:18 -0700

    PuppetAgain environment fixes

--- modules/toplevel/manifests/server/proxxy.pp
+++ modules/toplevel/manifests/server/proxxy.pp
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# FIXME: add 'inherits toplevel::server'
-class toplevel::server::proxxy {
+class toplevel::server::proxxy inherits toplevel::server {
     include ::proxxy
 }
